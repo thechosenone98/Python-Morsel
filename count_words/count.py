11b8d5447b0891bf89d10e60d924dc75b37791bb
Zacharie McCormick <zacfire1998@hotmail.fr>
2021-11-01 11:43:33 +0100

    Solved both bonuses of count_words

--- count_words/count.py
+++ count_words/count.py
@@ -1,7 +1,20 @@
 from collections import Counter
+from unicodedata import category as cat
+
+
+def strip_punctuation(word):
+    if cat(word[0]).startswith('P'):
+        return word[1:]
+    elif cat(word[-1]).startswith('P'):
+        return word[:-1]
+    return word
 
 def count_words(s: str):
-    return Counter(s.split())
+    # Split string into a word list
+    word_list = s.lower().split()
+    # Remove punctation outside of words
+    word_list = [strip_punctuation(word) for word in word_list]
+    return Counter(word_list)
 
 if __name__ == '__main__':
-    print(count_words("oh what a day what a lovely day"))
+    print(count_words("oh wha-t a, day what a lovely day"))
--- count_words/test_count.py
+++ count_words/test_count.py
@@ -34,14 +34,14 @@ class CountWordsTests(unittest.TestCase):
         self.assertEqual(actual, expected)
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    #@unittest.expectedFailure
     def test_capitalization(self):
         actual = count_words("Oh what a day what a lovely day")
         expected = {'oh': 1, 'what': 2, 'a': 2, 'day': 2, 'lovely': 1}
         self.assertEqual(actual, expected)
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    #@unittest.expectedFailure
     def test_symbols(self):
         actual = count_words("Oh what a day, what a lovely day!")
         expected = {'oh': 1, 'what': 2, 'a': 2, 'day': 2, 'lovely': 1}
